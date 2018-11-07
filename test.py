3799dda53ffa542e1edfc186201d2eebf07a6b19
Sudiptasaha47 <codename.sudipta@gmail.com>
2018-11-07 18:59:42 +0530

    Update test.py
    test

--- test.py
+++ test.py
@@ -1,9 +1,7 @@
 import socket
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.settimeout(2)                                      #2 Second Timeout
 result = sock.connect_ex(('127.0.0.1',80))
 if result == 0:
-  print 'port OPEN'
+   print "Port is open"
 else:
-  print 'port CLOSED, connect_ex returned: '+str(result)
+   print "Port is not open"
